7aac1d5c13038b1707165c0aef7cf3d89076931f
iman9922 <nano20021984@gmail.com>
2021-09-20 04:44:26 +0300

    this is finall projects

--- table.js
+++ table.js
@@ -1,5 +1,6 @@
 let number = 10;
 const table = document.querySelector("table");
+let show_nums = document.querySelector(".show_nums");
 let start_point = 0;
 
 let myTableTH = table.innerHTML;
@@ -8,8 +9,6 @@ let selecting = document.querySelector(".select");
 function getting_numbers() {
     selecting.addEventListener("change", function(event) {
         event.preventDefault();
-        let show_nums = document.querySelector(".show_nums");
-        show_nums.innerHTML = selecting.value;
 
         if (selecting.value == 10) {
             number = selecting.value;
@@ -18,26 +17,28 @@ function getting_numbers() {
         } else if (selecting.value == 50) {
             number = selecting.value;
         }
+        number = parseInt(number);
         start_point = 0;
-        table_information(number)
-
+        table_information(number);
 
     })
 }
 
 function handleControle(data, filteredCountries) {
+    handleControle = function() {};
     let left_i = document.getElementById('left_i');
+    let endEd = false;
     left_i.addEventListener("click", function(event) {
-        if (start_point > number - 1) {
-            start_point -= number - 1;
-            listItems(data, filteredCountries);
+        if (start_point >= number) {
+            start_point -= number;
+            endEd = listItems(data, filteredCountries);
         }
     })
     let right_i = document.getElementById('right_i');
     right_i.addEventListener("click", function(event) {
-        if (start_point < filteredCountries.length) {
-            start_point += number - 1;
-            listItems(data, filteredCountries);
+        if (!endEd) {
+            start_point += number;
+            endEd = listItems(data, filteredCountries);
         }
 
     })
@@ -66,15 +67,25 @@ function table_information(number) {
 
 function listItems(data, filteredCountries, num = number) {
     const [vaccination, cases] = data;
-    const countries = filteredCountries;
+    const countries = [].concat(filteredCountries);
     table.innerHTML = myTableTH;
-    for (let i = start_point; i < start_point + number; i++) {
-        const { abbreviation, administered, country, people_partially_vaccinated, people_vaccinated, population } = vaccination[countries[i]].All;
-        const { confirmed, deaths } = cases[countries[i]].All;
-        const valid_data = [abbreviation, administered, country, people_partially_vaccinated, people_vaccinated, population, confirmed, deaths].map(info => info ? info : "-")
-        table_data(...valid_data);
+    let i = start_point;
+    while ((i < (start_point + num)) && (i < countries.length - 1)) {
+        try {
+            const { abbreviation, administered, country, people_partially_vaccinated, people_vaccinated, population } = vaccination[countries[i]].All;
+            const { confirmed, deaths } = cases[countries[i]].All;
+            const valid_data = [abbreviation, administered, country, people_partially_vaccinated, people_vaccinated, population, confirmed, deaths].map(info => info ? info : "-")
+            table_data(...valid_data);
+
+        } catch (e) {
+            console.log(e);
+        }
+        i++;
         // searching_function(searching_data)
     }
+    show_nums.innerHTML = `${start_point+1}-${i}`;
+    return i >= countries.length - 1
+
 }
 let RECOVERD = [];
 let DEATH_MILION = [];
@@ -88,12 +99,12 @@ function table_data(abbreviation, administered, country, people_partially_vaccin
     
    
 <tr class="table_rows" onclick="goSite('${country}')">
-<td><img class="flags"src="https://lipis.github.io/flag-icon-css/flags/4x3/${abbreviation.toLowerCase()}.svg"><span class="country_name">${country}</span></td>
+<td class="td-start"><img class="flags"src="https://lipis.github.io/flag-icon-css/flags/4x3/${abbreviation.toLowerCase()}.svg"><span class="country_name">${country}</span></td>
 <td>${population.toLocaleString()}</td>
 <td>${confirmed.toLocaleString()}</td>
-<td>${RECOVERD}</td>
+<td> ${parseFloat(100*confirmed/population).toFixed(2)}% from population</td>
 <td>${deaths}</td>
-<td>${DEATH_MILION }</td>
+<td>${parseFloat(100*deaths/confirmed).toFixed(2)}% from cases</td>
 <td>${administered.toLocaleString()}</td>
 <td>${people_partially_vaccinated.toLocaleString() }</td>
 <td>${people_vaccinated.toLocaleString()}</td>
